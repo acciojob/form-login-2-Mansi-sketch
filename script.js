4287dd73fd3e4678634fc0be140b7727c7af0120
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-28 23:22:52 +0200

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function submit(){
+	let firstName = document.getElementById('firstName').value;
+	let lastName = document.getElementById('lastName').value;
+	let PhoneNumber = document.getElementById('PhoneNumber').value;
+	let email = document.getElementById('email').value;
+
+	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${PhoneNumber} Email ID: ${email}`);
+}
