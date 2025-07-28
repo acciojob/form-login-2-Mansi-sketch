a2d3fe592ed1ff141274c0a5b8f18699f3452213
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-28 23:28:36 +0200

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function submit(){
 	let firstName = document.getElementById('firstName').value;
 	let lastName = document.getElementById('lastName').value;
-	let PhoneNumber = document.getElementById('PhoneNumber').value;
+	let phoneNumber = document.getElementById('PhoneNumber').value;
 	let email = document.getElementById('email').value;
 
-	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${PhoneNumber} Email ID: ${email}`);
+	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`);
 }
